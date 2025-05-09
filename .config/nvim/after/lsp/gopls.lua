132979999a442f97781e8aa42c0143428998356e
ngicks <yknt.bsl@gmail.com>
2025-05-09 13:28:35 +0900

    restore go's format autocmd

--- .config/nvim/after/lsp/gopls.lua
+++ .config/nvim/after/lsp/gopls.lua
@@ -1,3 +1,36 @@
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function(args)
+    local clients = vim.lsp.get_clients { name = "gopls" }
+    if #clients == 0 then
+      return
+    end
+
+    local client = clients[1]
+
+    local pos_encoding = vim.lsp.get_client_by_id(client.id).offset_encoding or "utf-16"
+
+    local params = vim.lsp.util.make_range_params(vim.fn.bufwinid(args.buf), pos_encoding)
+    params = vim.tbl_deep_extend("force", params, { context = { only = { "source.organizeImports" } } })
+
+    -- buf_request_sync defaults to a 1000ms timeout. Depending on your
+    -- machine and codebase, you may want longer. Add an additional
+    -- argument after params if you find that you have to write the file
+    -- twice for changes to be saved.
+    -- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+    for cid, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+          vim.lsp.util.apply_workspace_edit(r.edit, enc)
+        end
+      end
+    end
+    vim.lsp.buf.format { async = false }
+  end,
+})
+
 return {
   settings = {
     gopls = { -- https://github.com/golang/tools/blob/master/gopls/doc/settings.md
