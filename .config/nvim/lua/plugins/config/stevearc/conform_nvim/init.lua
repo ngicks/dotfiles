4ccfea38ce45e874cad937ec5fd0374a5d20cb6a
ngicks <yknt.bsl@gmail.com>
2025-05-09 13:30:17 +0900

    fix conform option
    revisited https://github.com/stevearc/conform.nvim
    and noticed this configuration is incorrect

--- .config/nvim/lua/plugins/config/stevearc/conform_nvim/init.lua
+++ .config/nvim/lua/plugins/config/stevearc/conform_nvim/init.lua
@@ -9,11 +9,12 @@ M.opts = {
     json = { "prettier" },
     jsonc = { "prettier" },
   },
-
+  default_format_opts = {
+    lsp_format = "fallback",
+  },
   format_on_save = {
     -- These options will be passed to conform.format()
     timeout_ms = 500,
-    lsp_fallback = true,
   },
 }
 return M
