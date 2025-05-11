0fe79323a69da0c7e6ae14c88ca6e1cc9e5e197c
ngicks <yknt.bsl@gmail.com>
2025-05-12 03:50:00 +0900

    allow change gopls path and also temporary vendor fix for #3841
    https://github.com/neovim/nvim-lspconfig/issues/3841

--- .config/nvim/after/lsp/gopls.lua
+++ .config/nvim/after/lsp/gopls.lua
@@ -31,7 +31,51 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   end,
 })
 
+-- gopls from PATH, maybe maosn managed (= latest stable).
+local lsCmd = { "gopls" }
+
+if vim.env.GOPLS_PATH ~= nil and vim.env.GOPLS_PATH ~= "" then
+  -- Can be changed arbitrarily, maybe latest unreleased.
+  lsCmd = vim.env.GOPLS_PATH
+end
+
+local mod_cache = nil
+
+---@param fname string
+---@return string?
+local function get_root(fname)
+  if mod_cache and fname:sub(1, #mod_cache) == mod_cache then
+    local clients = vim.lsp.get_clients { name = "gopls" }
+    if #clients > 0 then
+      return clients[#clients].config.root_dir
+    end
+  end
+  return vim.fs.root(fname, { "go.work", "go.mod", ".git" })
+end
+
 return {
+  cmd = lsCmd,
+  root_dir = function(bufnr, on_dir)
+    local fname = vim.api.nvim_buf_get_name(bufnr)
+    -- see: https://github.com/neovim/nvim-lspconfig/issues/804
+    if mod_cache then
+      on_dir(get_root(fname))
+      return
+    end
+    local cmd = { "go", "env", "GOMODCACHE" }
+    vim.system(cmd, { text = true }, function(output)
+      if output.code == 0 then
+        if output.stdout then
+          mod_cache = vim.trim(output.stdout)
+        end
+        on_dir(get_root(fname))
+      else
+        vim.schedule(function()
+          vim.notify(("[gopls] cmd failed with code %d: %s\n%s"):format(output.code, cmd, output.stderr))
+        end)
+      end
+    end)
+  end,
   settings = {
     gopls = { -- https://github.com/golang/tools/blob/master/gopls/doc/settings.md
       analyses = {
@@ -72,7 +116,9 @@ return {
       semanticTokens = true,
       staticcheck = true,
       symbolMatcher = "fuzzy",
-      usePlaceholders = true,
+      -- I've used for a while with this option, and found it annoying.
+      -- Great feature tho.
+      usePlaceholders = false,
     },
   },
 }
