3d4d22a1fd822604109725f24e2c0317e7a19472
ngicks <yknt.bsl@gmail.com>
2025-05-26 19:33:29 +0900

    config/tmux: change color of host name in status bar based on uname -n

---
+++ .config/tmux/change_host_color.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+HOST_COLOR="$(uname -n | sha256sum | awk '{print substr($1, 1, 6)}')"
+
+r=0x${HOST_COLOR:0:2}
+g=0x${HOST_COLOR:2:2}
+b=0x${HOST_COLOR:4:2}
+brightness=$(((r*299 + g*587 + b*114)/1000))
+
+HOST_FG_COLOR=#FFFFFF
+if [ $brightness -gt 128 ]; then
+  HOST_FG_COLOR=#000000
+fi
+HOST_BG_COLOR=$HOST_COLOR
+
+tmux set-option -g status-right "#[fg=${HOST_FG_COLOR},bg=#${HOST_BG_COLOR}] #H #[fg=colour254,bg=colour241]| %Y-%m-%dT%H:%M:%S #[default]"
