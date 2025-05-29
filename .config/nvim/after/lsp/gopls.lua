196dc70617149f0caeacb2460b2cca0d2cff6b1c
ngicks <yknt.bsl@gmail.com>
2025-05-29 12:48:33 +0900

    config/nvim: Lazy sync + remove vendoered temporal fix

--- .config/nvim/after/lsp/gopls.lua
+++ .config/nvim/after/lsp/gopls.lua
@@ -39,43 +39,8 @@ if vim.env.GOPLS_PATH ~= nil and vim.env.GOPLS_PATH ~= "" then
   lsCmd = vim.env.GOPLS_PATH
 end
 
-local mod_cache = nil
-
----@param fname string
----@return string?
-local function get_root(fname)
-  if mod_cache and fname:sub(1, #mod_cache) == mod_cache then
-    local clients = vim.lsp.get_clients { name = "gopls" }
-    if #clients > 0 then
-      return clients[#clients].config.root_dir
-    end
-  end
-  return vim.fs.root(fname, { "go.work", "go.mod", ".git" })
-end
-
 return {
   cmd = lsCmd,
-  root_dir = function(bufnr, on_dir)
-    local fname = vim.api.nvim_buf_get_name(bufnr)
-    -- see: https://github.com/neovim/nvim-lspconfig/issues/804
-    if mod_cache then
-      on_dir(get_root(fname))
-      return
-    end
-    local cmd = { "go", "env", "GOMODCACHE" }
-    vim.system(cmd, { text = true }, function(output)
-      if output.code == 0 then
-        if output.stdout then
-          mod_cache = vim.trim(output.stdout)
-        end
-        on_dir(get_root(fname))
-      else
-        vim.schedule(function()
-          vim.notify(("[gopls] cmd failed with code %d: %s\n%s"):format(output.code, cmd, output.stderr))
-        end)
-      end
-    end)
-  end,
   settings = {
     gopls = { -- https://github.com/golang/tools/blob/master/gopls/doc/settings.md
       analyses = {
