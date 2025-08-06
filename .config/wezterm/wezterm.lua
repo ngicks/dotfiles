f12c7d705fe345f3e4b052d0ee3cdbcc7e62198d
ngicks <yknt.bsl@gmail.com>
2025-08-07 05:22:21 +0900

    config/wezterm: set up basic config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,24 +1,32 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
 
--- This table will hold the configuration.
-local config = {}
-
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
-if wezterm.config_builder then
-	config = wezterm.config_builder()
-end
+local config = wezterm.config_builder()
 
 if #(wezterm.default_wsl_domains()) > 0 then
 	config.default_domain = wezterm.default_wsl_domains()[1].name
 end
 
+-- remove windows bar
+config.window_decorations = "RESIZE"
+-- remove "+" button in tab bar
+config.show_new_tab_button_in_tab_bar = false
+
+-- fonts
 config.font = wezterm.font("BitstromWera Nerd Font Mono")
 config.font_size = 11
 
--- For example, changing the color scheme:
+-- color
 config.color_scheme = "Tokyo Night"
 
--- and finally, return the configuration to wezterm
+-- alerts
+-- Hate noises when pressing tab in shells.
+-- I might improve this later not just disabling all sounds, but disallow ringing bells when tabs/command fail, etc.
+config.audible_bell = "Disabled"
+
+-- keys
+config.leader = { key = " ", mods = "SHIFT", timeout_milliseconds = 1000 }
+
+config.keys = require("keybinds").keys
+config.key_tables = require("keybinds").key_tables
+
 return config
