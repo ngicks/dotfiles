b5794164a85d3cce366da32221245b1dca24a1ae
ngicks <yknt.bsl@gmail.com>
2025-08-07 05:24:37 +0900

    manager: don't call 2nd update:all if fist one succeeded

--- update_twice.sh
+++ update_twice.sh
@@ -2,5 +2,4 @@
 
 # If an update contains deno, proceeding deno task fails somehow with ENOENT.
 # Call twice to encounter that case.
-deno task update:all
-deno task update:all
+deno task update:all || deno task update:all
