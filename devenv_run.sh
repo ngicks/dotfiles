1003af4ac23da0d0e42ea35c1d18bfed01a5927e
ngicks <yknt.bsl@gmail.com>
2025-08-22 18:21:43 +0900

    manager: mount cert bundle

--- devenv_run.sh
+++ devenv_run.sh
@@ -10,9 +10,12 @@ if ! podman volume exists gemini-config; then
   podman volume create gemini-config
 fi
 
+SSL_CERT_FILE=${SSL_CERT_FILE:-/etc/ssl/certs/ca-certificates.crt}
+
 podman run -it --rm --init\
   --mount type=bind,src=$HOME/.config/env/,dst=/root/.config/env,ro\
   --mount type=bind,src=$HOME/.bashrc,dst=/root/.bashrc,ro\
+  --mount type=bind,src=${SSL_CERT_FILE},dst=${SSL_CERT_FILE},ro\
   --mount type=volume,src=claude-config,dst=/root/.config/claude\
   --mount type=volume,src=gemini-config,dst=/root/.gemini\
   --mount type=bind,src=.,dst=$(pwd)\
