5873c4c26577857e70353c7c4fc5739715c6fa16
ngicks <yknt.bsl@gmail.com>
2025-09-09 04:12:24 +0900

    config/initial_path: specify cache dir location

--- .config/initial_path/00_deno.sh
+++ .config/initial_path/00_deno.sh
@@ -1,1 +1,4 @@
 export DENO_NO_UPDATE_CHECK=1
+export DENO_DIR="$HOME/.local/deno/cache"
+# not documented but deno actually makes bin dir under DENO_INSTALL_ROOT.
+export DENO_INSTALL_ROOT="$HOME/.local/deno"
--- .config/initial_path/00_go.sh
+++ .config/initial_path/00_go.sh
@@ -1,2 +1,4 @@
+# I'm not using ~/.cache/go since it is somehow populated
 export GOPATH="$HOME/.local/go"
 export GOBIN="$HOME/.local/go/bin" 
+export PATH="$GOBIN:$PATH"
