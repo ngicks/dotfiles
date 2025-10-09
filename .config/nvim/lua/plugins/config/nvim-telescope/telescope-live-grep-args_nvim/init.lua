0f201779e36d78e87d639a91583151bfa24d01df
ngicks <yknt.bsl@gmail.com>
2025-10-09 03:56:23 +0000

    config/nvim: search hidden dirs in live grep

--- .config/nvim/lua/plugins/config/nvim-telescope/telescope-live-grep-args_nvim/init.lua
+++ .config/nvim/lua/plugins/config/nvim-telescope/telescope-live-grep-args_nvim/init.lua
@@ -2,11 +2,31 @@ local M = {}
 
 M.config = function()
   local telescope = require "telescope"
+  local telescope_config = require "telescope.config".values
   local lga_actions = require "telescope-live-grep-args.actions"
+  local unpack = table.unpack or unpack
+  local extra_glob_args = { "--hidden", "--glob", "!.git/**" }
+
+  local function vimgrep_with_hidden()
+    local args = { unpack(telescope_config.vimgrep_arguments) }
+    for _, flag in ipairs(extra_glob_args) do
+      table.insert(args, flag)
+    end
+    return args
+  end
+
+  local function hidden_args()
+    return { unpack(extra_glob_args) }
+  end
+
   telescope.setup {
+    defaults = {
+      vimgrep_arguments = vimgrep_with_hidden(),
+    },
     extensions = {
       live_grep_args = {
         auto_quoting = true, -- enable/disable auto-quoting
+        additional_args = hidden_args,
         -- define mappings, e.g.
         mappings = { -- extend mappings
           i = {
