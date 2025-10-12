3161023fccb0048ac37a221256fc40b29ac6c940
ngicks <yknt.bsl@gmail.com>
2025-10-12 13:53:15 +0900

    config/nvim: switch golangci-lint v1 and v2 based on config schema

--- .config/nvim/after/lsp/golangci_lint_ls.lua
+++ .config/nvim/after/lsp/golangci_lint_ls.lua
@@ -14,4 +14,66 @@ return {
     end
   end,
   root_markers = markers,
+  before_init = function(_, config)
+    -- switch version based on config schema.
+    -- golangci-lint v2 is relatively new.
+    -- So some projects still are sticking to v1,
+    -- while some other has been migrated to v2.
+
+    -- check v2 first since basically it is stricter
+    -- in some aspect, e.g. required version top element.
+    local v2 = vim
+      .system({
+        "mise",
+        "exec",
+        "go:github.com/golangci/golangci-lint/v2/cmd/golangci-lint",
+        "--",
+        "golangci-lint",
+        "config",
+        "verify",
+      })
+      :wait()
+
+    if v2.code == 0 then
+      config.init_options.command = {
+        "mise",
+        "exec",
+        "go:github.com/golangci/golangci-lint/v2/cmd/golangci-lint",
+        "--",
+        "golangci-lint",
+        "run",
+        "--output.json.path=stdout",
+        "--show-stats=false",
+      }
+      return
+    end
+
+    local v1 = vim
+      .system({
+        "mise",
+        "exec",
+        "go:github.com/golangci/golangci-lint/cmd/golangci-lint",
+        "--",
+        "golangci-lint",
+        "config",
+        "verify",
+      })
+      :wait()
+
+    if v1.code == 0 then
+      config.init_options.command = {
+        "mise",
+        "exec",
+        "go:github.com/golangci/golangci-lint/cmd/golangci-lint",
+        "--",
+        "golangci-lint",
+        "run",
+        "--out-format",
+        "json",
+      }
+      return
+    end
+
+    vim.notify '"golangci-lint config verify" failed for both v1 and v2'
+  end,
 }
