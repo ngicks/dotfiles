13b2f66f1c84f62365c0706430728577cb42603b
ngicks <yknt.bsl@gmail.com>
2025-10-18 16:45:16 +0900

    manager: source some script so that all caches are valid at runtime

--- mise_install.sh
+++ mise_install.sh
@@ -2,6 +2,14 @@
 
 set -Cue
 
+dir="$(dirname $0)/.config/initial_path"
+
+echo "sourcing $dir/00_*.sh"
+
+for f in $dir/00_*.sh; do
+  . $f
+done
+
 eval "$(mise activate $1)"
 core_tools=$(mise ls --json | jq -r 'keys[] | select(contains(":") | not)' | tr "\n" " ")
 echo "installing core(no backend) tools:"
