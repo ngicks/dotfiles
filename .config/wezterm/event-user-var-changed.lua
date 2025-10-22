f28e6aeb93d3e5fcb91577a712ca4231f5424680
ngicks <yknt.bsl@gmail.com>
2025-10-22 21:45:23 +0900

    config/wezterm: user-var-changed event also emits update-status

--- .config/wezterm/event-user-var-changed.lua
+++ .config/wezterm/event-user-var-changed.lua
@@ -3,7 +3,8 @@ local wezterm = require("wezterm")
 local M = {}
 
 M.handler = function(window, pane, name, value)
-	wezterm.log_info("var", name, value)
+	wezterm.log_info("user-var-changed", name, value)
+	wezterm.emit("update-status", window, pane, name, value)
 end
 
 return M
