c52497fb5b5e4053f309a8c8d245942d8bc71123
ngicks <yknt.bsl@gmail.com>
2025-11-02 19:56:56 +0900

    config/dotfiles_init: stop duplication

--- .config/dotfiles_init/00_kube.sh
+++ .config/dotfiles_init/00_kube.sh
@@ -1,1 +1,13 @@
-export KUBECONFIG="${KUBECONFIG}:$HOME/.config/kube/config"
+kube_config_loc=${XDG_CONFIG_HOME:-$HOME/.config}/kube/config
+case ":${KUBECONFIG}:" in
+    *:"${kube_config_loc}":*)
+        ;;
+    *)
+        if [[ -z ${KUBECONFIG} ]]; then
+            export KUBECONFIG="${kube_config_loc}"
+        else
+            export KUBECONFIG="${KUBECONFIG}:${kube_config_loc}"
+        fi
+        ;;
+esac
+
