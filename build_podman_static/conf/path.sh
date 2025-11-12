e7f2229edc1a2892328d1018b67c625a02297460
ngicks <yknt.bsl@gmail.com>
2025-11-12 09:03:35 +0900

    build_podman_satic: prevent path dupe

--- build_podman_static/conf/path.sh
+++ build_podman_static/conf/path.sh
@@ -1,2 +1,19 @@
-export PATH=$HOME/.local/containers/bin:$PATH
-export PATH=$HOME/.local/containers/lib/podman:$PATH
+c_bin=$HOME/.local/containers/bin:$PATH
+c_lib=$HOME/.local/containers/lib/podman:$PATH
+
+case ":${PATH}:" in
+    *:"c_bin":*)
+        ;;
+    *)
+        export PATH="c_bin:$PATH"
+        ;;
+esac
+
+
+case ":${PATH}:" in
+    *:"c_lib":*)
+        ;;
+    *)
+        export PATH="c_lib:$PATH"
+        ;;
+esac
