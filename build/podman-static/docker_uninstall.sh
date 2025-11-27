dc3106ec61c749433241fba3e041a9225d7d6c3c
ngicks <yknt.bsl@gmail.com>
2025-11-27 13:26:29 +0900

    build/podman-static: remove docker left-over on uninstalling

--- build/podman-static/docker_uninstall.sh
+++ build/podman-static/docker_uninstall.sh
@@ -5,3 +5,6 @@ set -Cue
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do
   sudo apt-get remove $pkg;
 done
+
+sudo rm -rf /var/lib/docker
+sudo rm -rf /var/lib/containerd
