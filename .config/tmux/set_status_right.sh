80f0f7f41bf0d7d4e475c9e213b0da307c6f7d3e
ngicks <yknt.bsl@gmail.com>
2025-12-10 20:05:23 +0900

    config/tmux: display zoom state on statusline

--- .config/tmux/set_status_right.sh
+++ .config/tmux/set_status_right.sh
@@ -1,4 +1,13 @@
 . ~/.config/tmux/color_scheme.sh
 
 tmux set-option -g status-right-length 100
-tmux set-option -g status-right "#[fg=${_tmux_color_text_light},bg=${_tmux_color_1}] %Y-%m-%dT%H:%M:%S #[default]"
+
+zoom_on_color="#[fg=${_tmux_color_text_dark}#,bg=${_tmux_color_5}]"
+zoom_off_color="#[fg=${_tmux_color_text_dark}#,bg=${_tmux_color_4}]"
+
+STATUS_RIGHT_ZOOM_FLAG="#{?window_zoomed_flag,${zoom_on_color}[Zoomed],${zoom_off_color}[normal]}#[default]"
+STATUS_RIGHT_TIMER="#[fg=${_tmux_color_text_light},bg=${_tmux_color_1}] %Y-%m-%dT%H:%M:%S #[default]"
+
+STATUS_RIGHT="${STATUS_RIGHT_ZOOM_FLAG}${STATUS_RIGHT_TIMER}"
+
+tmux set-option -g status-right "${STATUS_RIGHT}"
