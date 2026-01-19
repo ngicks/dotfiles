228517665bf942accc25b33c05fa16446351bcbf
ngicks <yknt.bsl@gmail.com>
2026-01-19 22:42:51 +0900

    config/nix: copied from https://zenn.dev/koba_e964/articles/32a7e0c345affe

--- nix-craft/flake.nix
+++ nix-craft/flake.nix
@@ -1,15 +1,37 @@
 {
-  description = "A very basic flake";
+    description = "dotfiles (home-manager, macOS)";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+        home-manager = {
+            url = "github:nix-community/home-manager";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+    };
 
-  outputs = { self, nixpkgs }: {
+    outputs = { self, nixpkgs, home-manager, ... }:
+    let
+        system = "aarch64-darwin";
+        pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfreePredicate = pkg:
+                builtins.elem (nixpkgs.lib.getName pkg) [
+                    "zsh-abbr"
+                ];
+        };
+    in
+    {
+        packages.${system}.home-manager = home-manager.packages.${system}.home-manager;
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+        apps.${system}.home-manager = {
+            type = "app";
+            program = "${self.packages.${system}.home-manager}/bin/home-manager";
+        };
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-  };
+        homeConfigurations.default =
+            home-manager.lib.homeManagerConfiguration {
+                inherit pkgs;
+                modules = [ ./home/home.nix ];
+            };
+    };
 }
