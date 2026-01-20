2487d6cead8eaeb9cb8949e11c469d5ffdf4f8bd
ngicks <yknt.bsl@gmail.com>
2026-01-21 08:58:17 +0900

    scripts/homeenv: mise up in container
    Because on host ~/.config/mise/* are symlinks to readonly files.
    Doing pivot_root by calling `podman container run` to swap
    ~/.config/mise to host's ~/.dotfiles/config/mise.

--- scripts/homeenv/mise-up.sh
+++ scripts/homeenv/mise-up.sh
@@ -2,7 +2,14 @@
 
 set -e
 
+tag=$(cat devenv_ver)
+
 pushd ./config/mise/
-  mise up
+  MISE_DATA_DIR=${MISE_DATA_DIR:-${XDG_DATA_HOME:-$HOME/.local/share}/mise}
+  podman container run -it --rm --init \
+    --mount type=bind,src=.,dst=/root/.config/mise \
+    --mount type=bind,src=${MISE_DATA_DIR},dst=/root/.local/share/mise \
+    localhost/devenv/devenv:${tag} \
+    -lc "mise up"
   mise lock
 popd
