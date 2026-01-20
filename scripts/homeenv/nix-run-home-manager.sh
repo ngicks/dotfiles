bfe7b8529891a6b9f05546863f7972affdf8bc92
ngicks <yknt.bsl@gmail.com>
2026-01-20 16:48:10 +0900

    scripts/homeenv: leave hint for reflesh

--- scripts/homeenv/nix-run-home-manager.sh
+++ scripts/homeenv/nix-run-home-manager.sh
@@ -10,3 +10,7 @@ fi
 
 cd ./nix-craft/
 nix run .#home-manager --extra-experimental-features "nix-command flakes" -- switch -b backup --flake .#default --impure --extra-experimental-features "nix-command flakes"
+
+# may need refresh
+
+# nix run .#home-manager --refresh --extra-experimental-features "nix-command flakes" -- switch -b backup --flake .#default --impure --extra-experimental-features "nix-command flakes"
