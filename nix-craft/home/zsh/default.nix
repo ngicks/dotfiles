24d620535ff57bedf8885732b7a5ed7bc117104a
ngicks <yknt.bsl@gmail.com>
2026-01-21 23:35:17 +0900

    nix: diplay username, hostname and error code from previous command

--- nix-craft/home/zsh/default.nix
+++ nix-craft/home/zsh/default.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 let
   loginscriptPath = ./loginscript;
 
@@ -59,6 +59,29 @@ let
       popd > /dev/null
     fi
   '';
+
+  # Hostname color script: computes color from hostname hash
+  hostnameColorScript = pkgs.writeShellScript "starship-hostname-color" ''
+    hostname=$(hostname -s)
+    hash=$(echo -n "$hostname" | sha256sum | cut -c1-6)
+
+    r=$((16#''${hash:0:2}))
+    g=$((16#''${hash:2:2}))
+    b=$((16#''${hash:4:2}))
+
+    # W3C AERT brightness formula
+    brightness=$(( (r * 299 + g * 587 + b * 114) / 1000 ))
+
+    # Background based on hostname color brightness (inverted)
+    if [ $brightness -gt 128 ]; then
+      bg="16;16;16"      # dark background for bright hostname color
+    else
+      bg="192;192;192"   # light background for dark hostname color
+    fi
+
+    # Output: "[ hostname ]" in computed color with inverted bg
+    printf '\033[48;2;%sm\033[38;2;%d;%d;%dm[ %s ]\033[48;2;118;159;240m\033[38;2;%sm\033[0m' "$bg" "$r" "$g" "$b" "$hostname" "$bg"
+  '';
 in
 {
   programs.zsh = {
@@ -93,7 +116,7 @@ in
 
     settings = {
       format = ''
-[░▒▓](#a3aed2)[  ](bg:#a3aed2 fg:#090c0c)[](bg:#769ff0 fg:#a3aed2)$directory[](fg:#769ff0 bg:#394260)$git_branch$git_status[](fg:#394260 bg:#212736)$nodejs$rust$golang$php[](fg:#212736 bg:#1d2230)$time[ ](fg:#1d2230)
+$status[](fg:#d5dee3 bg:#a3aed2)$username''${custom.hostname}$directory[](fg:#769ff0 bg:#394260)$git_branch$git_status[](fg:#394260 bg:#212736)$nodejs$rust$golang$php[](fg:#212736 bg:#1d2230)$time[ ](fg:#1d2230)
 $character
 '';
 
@@ -147,9 +170,32 @@ $character
 
       time = {
         disabled = false;
-        time_format = "%R";
+        time_format = "%T";
         style = "bg:#1d2230";
         format = "[[  $time ](fg:#a0a9cb bg:#1d2230)]($style)";
+      };
+
+      username = {
+        show_always = true;
+        style_root = "fg:#a36d15 bg:#a3aed2";
+        style_user = "fg:#315750 bg:#a3aed2";
+        format = "[ $user:]($style)";
+      };
+
+      # Custom hostname: [ hostname ] with computed colors
+      custom.hostname = {
+        command = "${hostnameColorScript}";
+        when = true;
+        format = "[$output](bg:#a3aed2 fg:#090c0c)";
+      };
+
+      # Exit code display
+      status = {
+        disabled = false;
+        symbol = "";
+        success_symbol = "";
+        format = "[[  $symbol $status ](fg:#e3e5e5 bg:#c53b53)]($style)[](fg:#c53b53 bg:#d5dee3)";
+        map_symbol = true;
       };
     };
   };
