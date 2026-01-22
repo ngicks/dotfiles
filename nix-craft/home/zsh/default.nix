bc9cb72a30ddab7eb651aeb2ce4c30c832de448c
ngicks <yknt.bsl@gmail.com>
2026-01-22 22:53:12 +0900

    nix: fix missing cdr

--- nix-craft/home/zsh/default.nix
+++ nix-craft/home/zsh/default.nix
@@ -90,6 +90,15 @@ in
       zle -N fzf-select-history
       bindkey '^r' fzf-select-history
 
+      autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
+      add-zsh-hook chpwd chpwd_recent_dirs
+
+      zstyle ':completion:*' recent-dirs-insert both
+      zstyle ':chpwd:*' recent-dirs-max 500
+      zstyle ':chpwd:*' recent-dirs-default true
+      zstyle ':chpwd:*' recent-dirs-file "$HOME/.cache/shell/chpwd-recent-dirs"
+      zstyle ':chpwd:*' recent-dirs-pushd true
+
       function fzf-cdr() {
         local selected_dir=''$(cdr -l | awk '{ print ''$2 }' | fzf --reverse)
         if [ -n "''$selected_dir" ]; then
