15b6e75607dc4add927c9e582aa84acdcc28bc52
ngicks <yknt.bsl@gmail.com>
2026-01-24 11:20:21 +0900

    scripts: fix mise-up

--- scripts/homeenv/mise-up.sh
+++ scripts/homeenv/mise-up.sh
@@ -3,15 +3,17 @@
 set -e
 
 tag=$(git describe --tags --abbrev=0 | cut -c 2-)
-runner=$(dirname $0)/run-devenv.sh
 
-pushd ./config/mise/
-  # Let mise up be called in container
-  # because it always tries to update things
-  # under `~/.config/mise/`.
-  runner \
-    "--mount type=bind,src=$HOME/.dotfiles/config/mise/,dst=/root/.config/mise \
-    --workdir /root/.config/mise" \
-    "-lc" "mise up && mise lock"
+runner=$(dirname $0)/run-devenv.sh
+# Let mise up be called in container
+# because it always tries to update things
+# under `~/.config/mise/`.
+$runner \
+  "--mount type=bind,src=$HOME/.dotfiles/config/mise/,dst=/root/.config/mise \
+  --workdir /root/.config/mise" \
+  "-lc" "mise up"
+
+pushd $(dirname $0)/../../config/mise
+  mise lock
 popd
 
