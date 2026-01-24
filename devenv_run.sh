f346422ecc17a590d3c39dec83f4a70faa6f5842
ngicks <yknt.bsl@gmail.com>
2026-01-25 03:08:47 +0900

    devenv: allow passing arg

--- devenv_run.sh
+++ devenv_run.sh
@@ -4,4 +4,8 @@ set -Cue
 
 runner=$(dirname $0)/scripts/homeenv/run-devenv.sh
 
-DEVENV_READONLY=1 $runner "--mount type=bind,src=.,dst=$(pwd) --workdir $(pwd)"
+first_arg=${1:-""}
+if [ "$#" -ge 1 ]; then
+  shift
+fi
+DEVENV_READONLY=1 $runner "--mount type=bind,src=.,dst=$(pwd) --workdir $(pwd) ${first_arg}" "$@"
