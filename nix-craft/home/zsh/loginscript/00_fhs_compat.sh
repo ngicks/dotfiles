5f79391789d5c20fc87c776fbcde09898e07fb0f
ngicks <yknt.bsl@gmail.com>
2026-01-27 19:52:27 +0900

    nix: remove fhs compat script

--- nix-craft/home/zsh/loginscript/00_fhs_compat.sh
+++
@@ -1,8 +0,0 @@
-# FHS binary compatibility: Set library paths for mounted binaries
-# Only needed if binaries require libraries not in their RPATH
-if [[ "${IN_CONTAINER:-}" == "1" ]]; then
-    NIX_PROFILE_LIB="$HOME/.nix-profile/lib"
-    if [[ -d "$NIX_PROFILE_LIB" ]]; then
-        export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:+$LD_LIBRARY_PATH:}$NIX_PROFILE_LIB"
-    fi
-fi
