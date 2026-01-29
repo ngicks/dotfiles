4efce10201d1539cea83703eefa731c609505fd5
ngicks <yknt.bsl@gmail.com>
2026-01-30 02:48:08 +0900

    config/tmux: move zoom flag to tab name

--- config/tmux/set_status_mid.sh
+++ config/tmux/set_status_mid.sh
@@ -1,6 +1,9 @@
 . ~/.config/tmux/color_scheme.sh
 
+zoom_on_color="#[fg=${_tmux_color_text_dark}#,bg=${_tmux_color_5}]"
+z_flag="#{?window_zoomed_flag,${zoom_on_color}[Z],}#[fg=colour254,bg=${_tmux_color_2}]"
+
 tmux set-option -g status-bg "${_tmux_color_0}"
 tmux set-option -g status-fg "colour255"
 tmux setw -g window-status-format "| #I: #W |"
-tmux setw -g window-status-current-format "#[fg=colour254,bg=${_tmux_color_2}]| #I: #W |"
+tmux setw -g window-status-current-format "#[fg=colour254,bg=${_tmux_color_2}]| #I: #W ${z_flag}|"
--- config/tmux/set_status_right.sh
+++ config/tmux/set_status_right.sh
@@ -2,12 +2,8 @@
 
 tmux set-option -g status-right-length 100
 
-zoom_on_color="#[fg=${_tmux_color_text_dark}#,bg=${_tmux_color_5}]"
-zoom_off_color="#[fg=${_tmux_color_text_dark}#,bg=${_tmux_color_4}]"
-
-STATUS_RIGHT_ZOOM_FLAG="#{?window_zoomed_flag,${zoom_on_color}[Zoomed],${zoom_off_color}[Normal]}#[default]"
 STATUS_RIGHT_TIMER="#[fg=${_tmux_color_text_light},bg=${_tmux_color_1}] %Y-%m-%dT%H:%M:%S #[default]"
 
-STATUS_RIGHT="${STATUS_RIGHT_ZOOM_FLAG}${STATUS_RIGHT_TIMER}"
+STATUS_RIGHT="${STATUS_RIGHT_TIMER}"
 
 tmux set-option -g status-right "${STATUS_RIGHT}"
