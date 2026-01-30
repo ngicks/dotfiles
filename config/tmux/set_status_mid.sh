4418d2240273dd688fbdc393e3e70af02d9e860e
ngicks <yknt.bsl@gmail.com>
2026-01-30 20:17:59 +0900

    config/tmux: add zoom flag to inactive tab

--- config/tmux/set_status_mid.sh
+++ config/tmux/set_status_mid.sh
@@ -5,5 +5,5 @@ z_flag="#{?window_zoomed_flag,${zoom_on_color}[Z],}#[fg=colour254,bg=${_tmux_col
 
 tmux set-option -g status-bg "${_tmux_color_0}"
 tmux set-option -g status-fg "colour255"
-tmux setw -g window-status-format "| #I: #W |"
+tmux setw -g window-status-format "| #I: #W #{?window_zoomed_flag,[Z],}|"
 tmux setw -g window-status-current-format "#[fg=colour254,bg=${_tmux_color_2}]| #I: #W ${z_flag}|"
