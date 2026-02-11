215bebe3fc9fd43a8c29d95370916e89bb8f67c8
ngicks <yknt.bsl@gmail.com>
2026-02-11 18:50:11 +0900

    devenv: remove mcp from prep

--- devenv/scripts/add_mcp_tools.sh
+++ devenv/scripts/add_mcp_tools.sh
@@ -2,6 +2,10 @@
 
 scope=${1:-"user"}
 
-claude mcp add --scope ${scope} serena -- serena start-mcp-server --context ide-assistant --project .
+
+# We don't use mcp anymore
+# But currently I am unable to replace context7.
+
+# claude mcp add --scope ${scope} serena -- serena start-mcp-server --context ide-assistant --project .
 claude mcp add --scope ${scope} context7 -- context7-mcp
-claude mcp add --scope ${scope} codex -- codex mcp-server
+# claude mcp add --scope ${scope} codex -- codex mcp-server
