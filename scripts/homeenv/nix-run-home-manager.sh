4c1c5230572777a3156434eb04cf9077ccb822fd
ngicks <yknt.bsl@gmail.com>
2026-02-17 03:22:56 +0900

    scripts/homeenv: do not remove dangling symlink
    It's been while

--- scripts/homeenv/nix-run-home-manager.sh
+++ scripts/homeenv/nix-run-home-manager.sh
@@ -2,12 +2,6 @@
 
 set -e
 
-config_dir="${XDG_CONFIG_HOME:-$HOME/.config}"
-if [[ -d "$config_dir" ]]; then
-  # `-xtype l` finds dangling symlink
-  find "$config_dir" -xtype l -delete
-fi
-
 cd ./nix-craft/
 nix run .#home-manager --extra-experimental-features "nix-command flakes" -- switch -b backup --flake .#default --impure --extra-experimental-features "nix-command flakes"
 
