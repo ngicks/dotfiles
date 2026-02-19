0c2df7fbb54abdd1835aa7ea9139e42e3576791d
ngicks <yknt.bsl@gmail.com>
2026-02-19 09:36:51 +0900

    agents: just copy .mcp.json

--- devenv/scripts/add_mcp_tools.sh
+++ devenv/scripts/add_mcp_tools.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-scope=${1:-"user"}
+cp $HOME/.dotfiles/agents/.mcp.json .
 
 
-# We don't use mcp anymore
-# But currently I am unable to replace context7.
-
-# claude mcp add --scope ${scope} serena -- serena start-mcp-server --context ide-assistant --project .
-claude mcp add --scope ${scope} context7 -- context7-mcp
-# claude mcp add --scope ${scope} codex -- codex mcp-server
