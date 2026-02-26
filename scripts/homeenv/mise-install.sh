24ff6188bf5e8ed15df1de477bd50144884126ac
ngicks <yknt.bsl@gmail.com>
2026-02-26 09:53:23 +0900

    scripts/homeenv: mise install in container
    mise.lock file is overwritten and home-manager is confused by this
    this change is intended to mitigate this problem

--- scripts/homeenv/mise-install.sh
+++ scripts/homeenv/mise-install.sh
@@ -7,3 +7,12 @@ pushd ./config/mise/
   mise install || true
   mise prune -y || true
 popd
+
+runner=$(dirname $0)/../../devenv/scripts/run-devenv.sh
+# Let mise up be called in container
+# because it always tries to update things
+# under `~/.config/mise/`.
+$runner \
+  "--workdir /" \
+  "-lc" "mise install || true && mise prune -y || true"
+
