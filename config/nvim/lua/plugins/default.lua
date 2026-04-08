df517580dc8cebc91126303da34734f12d4207be
ngicks <yknt.bsl@gmail.com>
2026-04-08 18:41:33 +0900

    config/nvim: (work around)set default version to head

--- config/nvim/lua/plugins/default.lua
+++ config/nvim/lua/plugins/default.lua
@@ -1,4 +1,6 @@
 ---@type NgPluginSpecDefaults
 return {
-  version = vim.version.range "*",
+  -- commented out until version comparison logic in vim.pack.add is fixed.
+  -- It always be thought needing to upate lock file.
+  -- version = vim.version.range "*",
 }
