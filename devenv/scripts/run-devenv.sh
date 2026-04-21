11774dedda15ecf421ec86f29719577be5c4f870
ngicks <yknt.bsl@gmail.com>
2026-04-22 05:58:24 +0900

    devenv/script: only mount pack

--- devenv/scripts/run-devenv.sh
+++ devenv/scripts/run-devenv.sh
@@ -46,7 +46,7 @@ fi
 SSL_CERT_FILE=${SSL_CERT_FILE:-/etc/ssl/certs/ca-certificates.crt}
 
 NVIM_STD_DATA=${XDG_DATA_HOME:-$HOME/.local/share}/nvim
-NVIM_CONFIG_DIR=${XDG_CONFIG_HOME:-$HOME/.config}/nvim
+NVIM_PACK_LOCK_FILE=$HOME/.dotfiles/config/nvim/nvim-pack-lock.json
 
 MISE_CONFIG_DIR=$HOME/.dotfiles/config/mise
 MISE_DATA_DIR=${MISE_DATA_DIR:-${XDG_DATA_HOME:-$HOME/.local/share}/mise}
@@ -96,9 +96,9 @@ podman container run -it --rm --init \
   ${timezone_opts}\
   \
   --mount type=bind,src=${XDG_CONFIG_HOME:-$HOME/.config}/env,dst=/root/.config/env,ro\
-  --mount type=bind,src=${NVIM_CONFIG_DIR},dst=/root/.config/nvim,ro\
   \
-  --mount type=bind,src=${NVIM_STD_DATA},dst=/root/.local/share/nvim\
+  --mount type=bind,src=${NVIM_PACK_LOCK_FILE},dst=/root/.config/nvim/nvim-pack-lock.json,ro\
+  --mount type=bind,src=${NVIM_STD_DATA},dst=/root/.local/share/nvim,ro\
   --mount type=bind,src=${NVIM_STD_DATA},dst=${NVIM_STD_DATA},ro\
   \
   --env MISE_GLOBAL_CONFIG_FILE=/root/.config/mise/mise.toml \
