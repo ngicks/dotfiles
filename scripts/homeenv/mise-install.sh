a1309af41c02f65564685f84adc94e2a9fcd28c1
ngicks <yknt.bsl@gmail.com>
2026-04-21 13:31:02 +0900

    scripts/homeenv: bulk re-install

--- scripts/homeenv/mise-install.sh
+++ scripts/homeenv/mise-install.sh
@@ -2,17 +2,11 @@
 
 set -e
 
-go_tools=(
-  "go:github.com/nametake/golangci-lint-langserver"
-  "go:github.com/golangci/golangci-lint/cmd/golangci-lint"
-  "go:github.com/golangci/golangci-lint/v2/cmd/golangci-lint"
-  "go:github.com/chrishrb/go-grip"
-  "go:github.com/mattn/memo"
-)
 
 pushd ./config/mise/
   # it always fails because of read-only lock file
   mise install
-  mise install -f "${go_tools[@]}"
+  mise install -f "go:*" || true
+  mise install -f "pipx:*" || true
   mise prune -y
 popd
--- scripts/homeenv/mise-up.sh
+++ scripts/homeenv/mise-up.sh
@@ -1,22 +1,12 @@
 #!/usr/bin/env bash
 
 runner=$(dirname $0)/../../devenv/scripts/run-devenv.sh
-# Let mise up be called in container
-# because it always tries to update things
-# under `~/.config/mise/`.
-go_tools=(
-  "go:github.com/nametake/golangci-lint-langserver"
-  "go:github.com/golangci/golangci-lint/cmd/golangci-lint"
-  "go:github.com/golangci/golangci-lint/v2/cmd/golangci-lint"
-  "go:github.com/chrishrb/go-grip"
-  "go:github.com/mattn/memo"
-)
 
 $runner \
   "--mount type=bind,src=$HOME/.dotfiles/config/mise/,dst=/mise \
   --env MISE_GLOBAL_CONFIG_FILE=/mise/mise.toml \
   --workdir /mise" \
-  "-lc" "mise up && mise install -f ${go_tools[*]} && mise prune -y"
+  "-lc" "mise up && mise install -f 'go:*' || true && mise install -f 'pipx:*' || true && mise prune -y"
 
 # Not sure, often it leaves old lock entries.
 
