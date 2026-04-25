3d633d5b6308d463cb393ab336b51465046ee610
ngicks <yknt.bsl@gmail.com>
2026-04-26 07:51:08 +0900

    👷(nix): move "mise activate" to zshenv

--- config/loginscript/env/00_mise.sh
+++ config/loginscript/env/00_mise.sh
@@ -1,1 +1,9 @@
 export_unless_container_override MISE_GLOBAL_CONFIG_FILE "${XDG_CONFIG_HOME:-$HOME/.config}/mise/mise.toml"
+
+if command -v mise > /dev/null 2>&1; then
+  if [ -n "${ZSH_VERSION:-}" ]; then
+    eval "$(mise activate zsh)"
+  elif [ -n "${BASH_VERSION:-}" ]; then
+    eval "$(mise activate bash)"
+  fi
+fi
