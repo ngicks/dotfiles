3b370aa6f386efa70aab2714518205eef7891f8c
ngicks <yknt.bsl@gmail.com>
2026-05-20 01:49:14 +0900

    🐛(scripts): possibly fix lock problem

--- scripts/homeenv/mise-up.sh
+++ scripts/homeenv/mise-up.sh
@@ -21,7 +21,9 @@ echo ""
 echo "mise install -f if missing"
 echo ""
 
-$run_in_new_shell "$HOME/.dotfiles/config/mise" $mise_install_f
+IN_CONTAINER=1 \
+  MISE_GLOBAL_CONFIG_FILE=$HOME/.dotfiles/config/mise/mise.toml \
+  $run_in_new_shell "$HOME/.dotfiles/config/mise" $mise_install_f
 
 echo ""
 echo "mise prune"
