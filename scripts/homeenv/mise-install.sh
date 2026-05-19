be8ff7c2e33bf394a14cf6c7a580c73d324dbb8c
ngicks <yknt.bsl@gmail.com>
2026-05-20 01:34:11 +0900

    🐛(scripts): possibly fix lock problem

--- scripts/homeenv/mise-install.sh
+++ scripts/homeenv/mise-install.sh
@@ -2,25 +2,23 @@
 
 set -euo pipefail
 
-run_in_container=$(cd $(dirname $0)/../../ && pwd -P)/devenv/scripts/run-devenv.sh
 run_in_new_shell=$(cd $(dirname $0) && pwd -P)/run-in-new-interactive-shell.sh
-
 mise_install_f=$(cd $(dirname $0) && pwd -P)/mise-install-f-if-missing.sh
 
 echo ""
 echo "mise install"
 echo ""
 
-$run_in_new_shell "$HOME/.dotfiles/config/mise" mise install
+IN_CONTAINER=1 MISE_GLOBAL_CONFIG_FILE=$HOME/.dotfiles/config/mise/mise.toml $run_in_new_shell "$HOME/.dotfiles/config/mise" mise install
 
 echo ""
 echo "mise install -f if missing"
 echo ""
 
-$run_in_new_shell "$HOME/.dotfiles/config/mise" $mise_install_f
+IN_CONTAINER=1 MISE_GLOBAL_CONFIG_FILE=$HOME/.dotfiles/config/mise/mise.toml $run_in_new_shell "$HOME/.dotfiles/config/mise" $mise_install_f
 
 echo ""
 echo "mise prune"
 echo ""
 
-$run_in_new_shell "$HOME/.dotfiles/config/mise" mise prune -y
+IN_CONTAINER=1 MISE_GLOBAL_CONFIG_FILE=$HOME/.dotfiles/config/mise/mise.toml $run_in_new_shell "$HOME/.dotfiles/config/mise" mise prune -y
