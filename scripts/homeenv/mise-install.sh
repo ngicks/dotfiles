ce603c894c3d0d131e890be13f6337bec75f7404
ngicks <yknt.bsl@gmail.com>
2026-05-20 01:48:58 +0900

    🧹: break lines

--- scripts/homeenv/mise-install.sh
+++ scripts/homeenv/mise-install.sh
@@ -9,16 +9,22 @@ echo ""
 echo "mise install"
 echo ""
 
-IN_CONTAINER=1 MISE_GLOBAL_CONFIG_FILE=$HOME/.dotfiles/config/mise/mise.toml $run_in_new_shell "$HOME/.dotfiles/config/mise" mise install
+IN_CONTAINER=1 \
+  MISE_GLOBAL_CONFIG_FILE=$HOME/.dotfiles/config/mise/mise.toml \
+  $run_in_new_shell "$HOME/.dotfiles/config/mise" mise install
 
 echo ""
 echo "mise install -f if missing"
 echo ""
 
-IN_CONTAINER=1 MISE_GLOBAL_CONFIG_FILE=$HOME/.dotfiles/config/mise/mise.toml $run_in_new_shell "$HOME/.dotfiles/config/mise" $mise_install_f
+IN_CONTAINER=1 \
+  MISE_GLOBAL_CONFIG_FILE=$HOME/.dotfiles/config/mise/mise.toml \
+  $run_in_new_shell "$HOME/.dotfiles/config/mise" $mise_install_f
 
 echo ""
 echo "mise prune"
 echo ""
 
-IN_CONTAINER=1 MISE_GLOBAL_CONFIG_FILE=$HOME/.dotfiles/config/mise/mise.toml $run_in_new_shell "$HOME/.dotfiles/config/mise" mise prune -y
+IN_CONTAINER=1 \
+  MISE_GLOBAL_CONFIG_FILE=$HOME/.dotfiles/config/mise/mise.toml \
+  $run_in_new_shell "$HOME/.dotfiles/config/mise" mise prune -y
