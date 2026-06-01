2413849c3b90b87c37baf6cede2e0ab50b9e6bdd
ngicks <yknt.bsl@gmail.com>
2026-06-02 02:02:25 +0900

    👷: install nvim pack again
    Often I open a neovim session after homeup / homesync
    It displays downloading deps and it's ok after restart
    
    So with this commit, I intend to fix it by just pack.add again

--- scripts/homeenv/nvim-pack-update.sh
+++ scripts/homeenv/nvim-pack-update.sh
@@ -14,3 +14,10 @@ nvim --headless \
   "+lua vim.pack.update()" \
   "+write" \
   "+quitall"
+
+XDG_CONFIG_HOME="$dir/config" \
+XDG_CACHE_HOME="${XDG_CACHE_HOME:-/tmp/nvim-cache}" \
+XDG_STATE_HOME="${XDG_STATE_HOME:-/tmp/nvim-state}" \
+nvim --headless \
+  "+lua vim.pack.add(require(\"ngpack\").list_pack(), { confirm = false })" \
+  "+quitall"
