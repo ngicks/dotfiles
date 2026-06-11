66f022b8314cab63675c957ba8f302c9a8b49ea9
ngicks <yknt.bsl@gmail.com>
2026-06-12 03:22:21 +0900

    ✨(devenv): mount ~/blobs/os-image as readonly

--- devenv/scripts/opts.d/10-core.sh
+++ devenv/scripts/opts.d/10-core.sh
@@ -11,6 +11,7 @@ printf "%s\n" "--env SSL_CERT_FILE=${SSL_CERT_FILE}"
 printf "%s\n" "--mount type=bind,src=${SSL_CERT_FILE},dst=/etc/ssl/certs/ca-certificates.crt,ro"
 
 printf "%s\n" "--mount type=bind,src=${XDG_CONFIG_HOME:-$HOME/.config}/env,dst=/root/.config/env,ro"
+printf "%s\n" "--mount type=bind,src=$HOME/blobs/os-iamge,dst=/root/blobs/os-image,ro"
 
 printf "%s\n" "--env XDG_RUNTIME_DIR=/run/user/1000/"
 printf "%s\n" "--mount type=tmpfs,dst=/run/user/1000/,tmpfs-size=10m"
