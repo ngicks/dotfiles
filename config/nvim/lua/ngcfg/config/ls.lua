b4533e1c4462fc73717831e7876bbeb97e76c4cb
ngicks <yknt.bsl@gmail.com>
2026-06-29 15:18:27 +0900

    🐛(nvim): seemingly moonbit-lsp's root detection is wrong

--- config/nvim/lua/ngcfg/config/ls.lua
+++ config/nvim/lua/ngcfg/config/ls.lua
@@ -12,6 +12,10 @@ local nonCustomizedServers = {
   "moonbit-lsp",
 }
 
+local externallyHandledServers = {
+  ["moonbit-lsp"] = true,
+}
+
 local function get_servers()
   local servers = {}
 
@@ -22,7 +26,7 @@ local function get_servers()
   for _, fileName in ipairs(files) do
     local name = vim.fs.basename(fileName)
     local serverName, occurence = name:gsub("%.lua", "")
-    if occurence == 1 then -- Could there be .lua.lua files?
+    if occurence == 1 and not externallyHandledServers[serverName] then -- Could there be .lua.lua files?
       table.insert(servers, serverName)
     end
   end
--- config/nvim/lua/ngcfg/plugins/config/github_com--moonbit-community--moonbit_nvim.lua
+++ config/nvim/lua/ngcfg/plugins/config/github_com--moonbit-community--moonbit_nvim.lua
@@ -1,10 +1,22 @@
 ---@type NgPackPluginConfigModule
 local M = {}
 
+local root_markers = { "moon.mod.json", "moon.mod" }
+
 M.opts = {
   treesitter = {
     enabled = true,
     auto_install = true,
+  },
+  lsp = {
+    native = true,
+    root_markers = root_markers,
+    root_dir = function(bufnr, on_dir)
+      local root = vim.fs.root(bufnr, root_markers)
+      if root ~= nil and root ~= "" then
+        on_dir(root)
+      end
+    end,
   },
 }
 
