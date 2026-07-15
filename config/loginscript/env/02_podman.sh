66073ccb59dfb3a5d89b29e88a82e6623f404c13
ngicks <yknt.bsl@gmail.com>
2026-07-16 04:24:15 +0900

    ✨: define addtional quadlet dir and let normal shell quadlet see the dirs

--- config/loginscript/env/02_podman.sh
+++ config/loginscript/env/02_podman.sh
@@ -1,4 +1,11 @@
 export PODMAN_IGNORE_CGROUPSV1_WARNING=1
+
+# keep in sync with ../../environment.d/75-podman.conf
+_quadlet_additional="${XDG_CONFIG_HOME:-$HOME/.config}/containers-quadlet-additional"
+mkdir -p "${_quadlet_additional}"
+export_unless_container_override QUADLET_UNIT_DIRS "${XDG_CONFIG_HOME:-$HOME/.config}/containers-quadlet:${_quadlet_additional}"
+unset _quadlet_additional
+
 if [[ -f $HOME/.config/containers/path.sh ]]; then
   . $HOME/.config/containers/path.sh
 fi
