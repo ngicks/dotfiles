f3b9c718853c0b91757080fe1c90e032cfb8cb52
ngicks <yknt.bsl@gmail.com>
2026-08-02 13:09:44 +0900

    🔥: remove unused

--- src/devenv_build.ts
+++
@@ -1,8 +0,0 @@
-import { build } from "#/devenv/build.ts";
-
-async function main() {
-  const exp = Deno.args.includes("--exp") || Deno.args.includes("-e");
-  await build(exp);
-}
-
-main();
--- src/devenv_save.ts
+++
@@ -1,9 +0,0 @@
-import { save } from "#/devenv/save.ts";
-
-async function main() {
-  const exp = Deno.args.includes("--exp") || Deno.args.includes("-e");
-  const noBuild = Deno.args.includes("--no-build");
-  await save(noBuild, exp);
-}
-
-main();
