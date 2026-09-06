0a6ffb651014c6ad934371c43555f05005bbf611
ngicks <yknt.bsl@gmail.com>
2026-09-06 19:11:30 +0900

    📝(homeenv): add reference

--- config/loginscript/env/01_llm.sh
+++ config/loginscript/env/01_llm.sh
@@ -1,2 +1,3 @@
 export_unless_container_override CLAUDE_CONFIG_DIR "${XDG_CONFIG_HOME:-$HOME/.config}/claude"
+# https://x.com/kawasin73/status/2096021862019145786?s=20
 export_unless_container_override CLAUDE_CODE_THRIFTY_SONIC false
